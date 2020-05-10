bb00e6c71f558482162ddd3ab00634e4caae8e6a
ian1017 <mdawidamengo@gmail.com>
2020-05-11 00:52:16 +0300

    Add overlay

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
 
     public showDescription: boolean;
-    constructor(public id: number,public description: string,public name: string, public completeDate: Date){
+    constructor(public id: number,public description: string,public name: string, public completeDate: Date, upvotes:number,downvotes:number){
       this.showDescription=false;
     }
 }
