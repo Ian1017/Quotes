6e1fee7829b821b80a9a917c2101b323b63145cd
ian1017 <mdawidamengo@gmail.com>
2020-05-11 02:02:06 +0300

    Add minor changes and error fixations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule,  CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 
@@ -14,6 +14,7 @@ import { LikeComponent } from './like/like.component';
 @NgModule({
   declarations: [
     AppComponent ,
+    QuoteComponent,
     QuoteDetailsComponent,
     DateCountPipe,
     QuoteFormComponent,
--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -10,8 +10,8 @@ export class QuoteDetailsComponent implements OnInit {
 
   @Input() quote: Quote;
   @Output() isComplete = new  EventEmitter<boolean>();
-  
-  quoteComplete(complete:boolean){
+
+  quoteDelete(complete:boolean){
     this.isComplete.emit(complete);
   } 
   constructor() { }
