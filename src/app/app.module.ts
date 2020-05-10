e407a9178b9c9f421efe42504ad54f7506a7d017
ian1017 <mdawidamengo@gmail.com>
2020-05-10 23:41:02 +0300

    Toogle button functional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
-import { QuoteDetailsComponent } from '.quote-details/quote-details.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-    QuoteComponent,
+    AppComponent ,
     QuoteDetailsComponent,
   ],
   imports: [
--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -8,6 +8,11 @@ import { Quote } from '../quote';
 })
 export class QuoteDetailsComponent implements OnInit {
 
+  @Input() quote: Quote;
+  @Output() isComplete = new  EventEmitter<boolean>();
+  goalComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  } 
   constructor() { }
 
   ngOnInit(): void {
